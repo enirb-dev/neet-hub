a05e4f425a133d7a916fe83417c57b16704e120d
Siddharth <enirb.dev@gmail.com>
2026-09-19 20:12:02 +0530

    CLI-Update

--- js/script.js
+++ js/script.js
@@ -406,7 +406,7 @@ onAuthStateChanged(
 		
 		
 		if (currentUsername == "admin") {
-			emergencyEnabled = false;
+			if (emergencyEnabled) { toggleEmergencyActivity(); }
 			GE("loginPage").style.display = "none";
 			GE("chatPage").style.display = "none";
 			GE("adminPage").style.display = "block";
